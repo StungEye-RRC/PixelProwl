a169845b0256f6355cf939289e1f8d0bd18ab909
Kyle Geske <stungeye@gmail.com>
2023-08-31 11:58:22 -0500

    Initial commit of pixel prowl unreal project.

---
+++ Source/PixelProwl/PixelProwl.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PixelProwl.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PixelProwl, "PixelProwl" );
---
+++ Source/PixelProwl/PixelProwlGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PixelProwlGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXELPROWL_API APixelProwlGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
